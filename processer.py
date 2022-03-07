6d3ac7967916f3ea1d710651d8f64fe28813a4d2
dan <daniel.j.menz@gmail.com>
2022-03-07 22:14:48 +1100

    single data file

--- processer.py
+++ processer.py
@@ -4,60 +4,61 @@ import pandas as pd
 import numpy as np
 import time
 import json
+import sys
 from simple_term_menu import TerminalMenu, main
 #from dask import dataframe as df1
   
 # time taken to read data
 
-generate_new_percentile_tables = True
+generate_new_percentile_tables = False
+#generate_untested = True
 
-if generate_new_percentile_tables:
-    s_time = time.time()
-    pd.set_option('display.max_columns', None)
-    #df = pd.read_csv("openpowerlifting-2022-02-22-8fc7c9ba.csv")
-    df = pd.read_csv("openipf-2022-02-22-8fc7c9ba.csv")
-    e_time = time.time()
-    print("Read without chunks: ", (e_time-s_time), "seconds")
-  
+weight_class_dict = {
+    '53_M' : {"WeightClass": "53", "Sex" : 'M', "Equip": "raw", "Tested": "tested", "WeightMin": 0.0, "WeightMax": 53.0},
+    '59_M' : {"WeightClass": "59", "Sex" : 'M', "Equip": "raw", "Tested": "tested",  "WeightMin": 53.0, "WeightMax": 59.0},
+    '66_M' : {"WeightClass": "66", "Sex" : 'M', "Equip": "raw", "Tested": "tested",  "WeightMin": 59.0, "WeightMax": 66.0},
+    '74_M' : {"WeightClass": "74", "Sex" : 'M', "Equip": "raw", "Tested": "tested",  "WeightMin": 66.0, "WeightMax": 74.0},
+    '83_M' : {"WeightClass": "83", "Sex" : 'M', "Equip": "raw", "Tested": "tested",  "WeightMin": 74.0, "WeightMax": 83.0},
+    '93_M' : {"WeightClass": "93", "Sex" : 'M', "Equip": "raw", "Tested": "tested",  "WeightMin": 83.0, "WeightMax": 93.0},
+    '105_M' : {"WeightClass": "105", "Sex" : 'M', "Equip": "raw", "Tested": "tested",  "WeightMin": 93.0, "WeightMax": 105.0},
+    '120_M' : {"WeightClass": "120", "Sex" : 'M', "Equip": "raw", "Tested": "tested",  "WeightMin": 105.0, "WeightMax": 120.0},
+    '120+_M' : {"WeightClass": "120+", "Sex" : 'M', "Equip": "raw", "Tested": "tested",  "WeightMin": 120.0, "WeightMax": 999.0},
+    '43_F' : {"WeightClass": "43", "Sex" : 'F', "Equip": "raw", "Tested": "tested",  "WeightMin": 0.0, "WeightMax": 43.0},
+    '47_F' : {"WeightClass": "47", "Sex" : 'F', "Equip": "raw", "Tested": "tested",  "WeightMin": 43.0, "WeightMax": 47.0},
+    '52_F' : {"WeightClass": "52", "Sex" : 'F', "Equip": "raw", "Tested": "tested",  "WeightMin": 47.0, "WeightMax": 52.0},
+    '57_F' : {"WeightClass": "57", "Sex" : 'F', "Equip": "raw", "Tested": "tested",  "WeightMin": 52.0, "WeightMax": 57.0},
+    '63_F' : {"WeightClass": "63", "Sex" : 'F', "Equip": "raw", "Tested": "tested",  "WeightMin": 57.0, "WeightMax": 63.0},
+    '72_F' : {"WeightClass": "72", "Sex" : 'F', "Equip": "raw", "Tested": "tested",  "WeightMin": 63.0, "WeightMax": 72.0},
+    '84_F' : {"WeightClass": "84", "Sex" : 'F', "Equip": "raw", "Tested": "tested",  "WeightMin": 72.0, "WeightMax": 84.0},
+    '84+_F' : {"WeightClass": "84+", "Sex" : 'F', "Equip": "raw", "Tested": "tested",  "WeightMin": 84.0, "WeightMax": 999.0},
+}
 
+weight_class_dict_untested = {
+    '52_M' : {"WeightClass": "52", "Sex" : 'M', "Equip": "raw", "Tested": "untested", "WeightMin": 0.0, "WeightMax": 52.0},
+    '56_M' : {"WeightClass": "56", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 52.0, "WeightMax": 56.0},
+    '60_M' : {"WeightClass": "60", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 56.0, "WeightMax": 60.0},
+    '67.5_M' : {"WeightClass": "67.5", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 60.0, "WeightMax": 67.5},
+    '75_M' : {"WeightClass": "75", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 67.5, "WeightMax": 75.0},
+    '82.5_M' : {"WeightClass": "82.5", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 75.0, "WeightMax": 82.5},
+    '90_M' : {"WeightClass": "90", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 82.5, "WeightMax": 90.0},
+    '100_M' : {"WeightClass": "100", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 90.0, "WeightMax": 100.0},
+    '110_M' : {"WeightClass": "110", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 100.0, "WeightMax": 110.0},
+    '125_M' : {"WeightClass": "125", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 110.0, "WeightMax": 125.0},
+    '140_M' : {"WeightClass": "140", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 125.0, "WeightMax": 140.0},
+    '140+_M' : {"WeightClass": "140+", "Sex" : 'M', "Equip": "raw", "Tested": "untested",  "WeightMin": 140.0, "WeightMax": 999.0},
+    '44_F' : {"WeightClass": "44", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 0.0, "WeightMax": 44.0},
+    '48_F' : {"WeightClass": "48", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 44.0, "WeightMax": 48.0},
+    '52_F' : {"WeightClass": "52", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 48.0, "WeightMax": 52.0},
+    '56_F' : {"WeightClass": "56", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 52.0, "WeightMax": 56.0},
+    '60_F' : {"WeightClass": "60", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 56.0, "WeightMax": 60.0},
+    '67.5_F' : {"WeightClass": "67.5", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 60.0, "WeightMax": 67.5},
+    '75_F' : {"WeightClass": "75", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 67.5, "WeightMax": 75.0},
+    '82.5_F' : {"WeightClass": "82.5", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 75.0, "WeightMax": 82.5},
+    '90_F' : {"WeightClass": "90", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 82.5, "WeightMax": 90.0},
+    '90+_F' : {"WeightClass": "90+", "Sex" : 'F', "Equip": "raw", "Tested": "untested",  "WeightMin": 90.0, "WeightMax": 999.0},
+}
 
-def generate_tables():
-    pass
-
-# data
-#print(df.sample(50))
-#print(df.columns.tolist())
-#print(len(df))
-
-#me = df[df['Name'] == 'Daniel Menz']
-#print(me)
-
-
-#filter_weight_class = '74' 
-#filter_lift = 'TotalKg' #Best3SquatKg, Best3BenchKg, Best3DeadliftKg TotalKg
-#target_lift = 501 # your inputted lift
-
-
-
-
-
-def calc_percentile(weight_class_full_data, filter_lift_input, target_lift, ):
-    class_percentiles = {
-        "Squat" : {},
-        "Bench" : {},
-        "Deadlift": {},
-        "Total": {}
-    }
-
-    for filter_lift in ['Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg', 'TotalKg']:
-        filter_event_dict = {
-            'Best3SquatKg': ['SBD'],
-            'Best3BenchKg': ['B','BD','SBD'],
-            'Best3DeadliftKg': ['D','BD','SBD'],
-            'TotalKg': ['SBD']
-        }
-
-        youth_divisions = [
+youth_divisions = [
         'MR-T1',
         'MR-T2',
         'MR-T3',
@@ -74,59 +75,129 @@ def calc_percentile(weight_class_full_data, filter_lift_input, target_lift, ):
         'Teen',
         ]
 
-        filter_event = filter_event_dict[filter_lift]
+lift_label_lookup = {
+                'Best3SquatKg': 'Squat',
+                'Best3BenchKg': 'Bench',
+                'Best3DeadliftKg': 'Deadlift',
+                'TotalKg': 'Total'
+            }
 
-        res = []
+def generate_tables():
+    tested_data_csv = "openipf-2022-02-22-8fc7c9ba.csv"
+    untested_data_csv = "openpowerlifting-2022-02-22-8fc7c9ba.csv"    
+    generate_tables_per_file(tested_data_csv, weight_class_dict)
+    generate_tables_per_file(untested_data_csv, weight_class_dict_untested)
 
-        filtered_df = df[(df['Equipment'] == 'Raw')  
-                    & (df['BodyweightKg'] > weight_class_full_data['WeightMin']) 
-                    & (df['BodyweightKg'] <= weight_class_full_data['WeightMax'])  
-                    & (df['Equipment'] == 'Raw')  
-                    & (df['Sex'] == weight_class_full_data['Sex'])
-                    & (df['Event'].isin(filter_event))
-                    & (~df['Division'].isin(youth_divisions))    
-                    & (np.isnan(df[filter_lift]) == False)]
+def generate_tables_per_file(csv_file, weight_classes):
 
-        #print(len(filtered_df))
-        selection = filtered_df.loc[:,['Name','BodyweightKg', 'Event', filter_lift, 'Age']]
-
-        # Need to get the best total for each Name
-
-
-        sorted_df = selection.sort_values(filter_lift, ascending=False).drop_duplicates(['Name'])
-        entry_size = len(sorted_df)
-        print(f"{entry_size} entries in selected class") # Number of entries in that category, not name disambiguated
-        print(filter_lift)
-        print(weight_class_full_data['WeightClass'])
-
-        # percentiles = [100, 90, 80, 70, 60, 50 ,40 ,30 ,20, 10]
-
-        percentiles = [i for i in range(100,0, -1)]
-
-        for percentile in percentiles:
-            location = int(entry_size * (1 - (percentile / 100)))
-            entry = (sorted_df.iloc[location,:])
-            print(f"{percentile} - {entry[filter_lift]} - {entry['Name']} - {entry_size - int(percentile/100*entry_size)}")
-            res.append((percentile, entry[filter_lift]))
-
-        lift_label_lookup = {
-            'Best3SquatKg': 'Squat',
-            'Best3BenchKg': 'Bench',
-            'Best3DeadliftKg': 'Deadlift',
-            'TotalKg': 'Total'
+    s_time = time.time()
+    pd.set_option('display.max_columns', None)
+    #df = pd.read_csv("openpowerlifting-2022-02-22-8fc7c9ba.csv")
+    df = pd.read_csv(csv_file)
+    e_time = time.time()
+    print("Read without chunks: ", (e_time-s_time), "seconds")
+    
+    for weight_class_full_data in weight_classes.values():
+                
+        class_percentiles = {
+            "Squat" : {},
+            "Bench" : {},
+            "Deadlift": {},
+            "Total": {}
         }
 
-        class_percentiles[lift_label_lookup[filter_lift]] = res
-        print()   
+        for filter_lift in ['Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg', 'TotalKg']:
+            filter_event_dict = {
+                'Best3SquatKg': ['SBD'],
+                'Best3BenchKg': ['B','BD','SBD'],
+                'Best3DeadliftKg': ['D','BD','SBD'],
+                'TotalKg': ['SBD']
+            }
 
-    # Output to file
-    output_file_name = weight_class_full_data['WeightClass'] +'_percentiles.json'
-    with open(output_file_name, "w") as outfile:
-        outfile.write(json.dumps(class_percentiles))
+            filter_event = filter_event_dict[filter_lift]
 
-    #print(class_percentiles)
+            res = []
 
-    for pair in class_percentiles[lift_label_lookup[filter_lift_input]]:
+            filtered_df = df[(df['Equipment'] == 'Raw')  
+                        & (df['BodyweightKg'] > weight_class_full_data['WeightMin']) 
+                        & (df['BodyweightKg'] <= weight_class_full_data['WeightMax'])  
+                        & (df['Equipment'] == 'Raw')  
+                        & (df['Sex'] == weight_class_full_data['Sex'])
+                        & (df['Event'].isin(filter_event))
+                        & (~df['Division'].isin(youth_divisions))    
+                        & (np.isnan(df[filter_lift]) == False)]
+
+            #print(len(filtered_df))
+            selection = filtered_df.loc[:,['Name','BodyweightKg', 'Event', filter_lift, 'Age']]
+
+            # Need to get the best total for each Name
+
+
+            sorted_df = selection.sort_values(filter_lift, ascending=False).drop_duplicates(['Name'])
+            entry_size = len(sorted_df)
+            print(f"{entry_size} entries in selected class") # Number of entries in that category, not name disambiguated
+            print(filter_lift)
+            print(weight_class_full_data['WeightClass'])
+
+            # percentiles = [100, 90, 80, 70, 60, 50 ,40 ,30 ,20, 10]
+
+            percentiles = [i for i in range(100,0, -1)]
+
+            for percentile in percentiles:
+                location = int(entry_size * (1 - (percentile / 100)))
+                entry = (sorted_df.iloc[location,:])
+                print(f"{percentile} - {entry[filter_lift]} - {entry['Name']} - {entry_size - int(percentile/100*entry_size)}")
+                res.append([percentile, entry[filter_lift], entry['Name'], entry_size - int(percentile/100*entry_size)])
+                #res.append([percentile, entry[filter_lift]])
+
+
+            class_percentiles[lift_label_lookup[filter_lift]] = res
+            print()
+
+        # Output to file
+        output_file_name = weight_class_full_data['Sex'] + weight_class_full_data['WeightClass'] +weight_class_full_data['Equip'] \
+                         + '_percentiles.json'
+        with open('data/' + output_file_name, "w") as outfile:
+            outfile.write(json.dumps(class_percentiles))
+
+        # output to one file (read in current and then add to dict and append to file)
+        with open('all_data') as file:
+            cur_percentile_data = json.load(file)
+
+        cur_percentile_data[weight_class_full_data['Sex'] + weight_class_full_data['WeightClass'] +weight_class_full_data['Equip']] = class_percentiles   
+
+        with open('all_data', "w") as outfile:
+            outfile.write(json.dumps(cur_percentile_data))
+    
+    pass
+
+# data
+#print(df.sample(50))
+#print(df.columns.tolist())
+#print(len(df))
+#me = df[df['Name'] == 'Daniel Menz']
+#print(me)
+
+def calc_percentile(weight_class_full_data, filter_lift_input, target_lift, ):
+   
+    # load in correct file
+    input_file_name = weight_class_full_data['Sex'] + weight_class_full_data['WeightClass'] + weight_class_full_data['Equip'] \
+                         + '_percentiles.json'
+
+    class_dict_key = weight_class_full_data['Sex'] + weight_class_full_data['WeightClass'] + weight_class_full_data['Equip']
+    
+    with open('data/' + input_file_name) as file:
+        percentile_data = json.load(file)
+
+    with open('all_data') as file:
+            all_percentile_data = json.load(file)
+
+    for data in (all_percentile_data[class_dict_key][lift_label_lookup[filter_lift_input]]):
+        print(data)
+
+    print('------------')
+
+    for pair in percentile_data[lift_label_lookup[filter_lift_input]]:
         print(pair)
         if pair[1] > target_lift:
             pass
@@ -137,30 +208,17 @@ def calc_percentile(weight_class_full_data, filter_lift_input, target_lift, ):
 
 
 def main():
+    generate_new_percentile_tables = False
 
+    if len(sys.argv) == 2 and sys.argv[1] == 'gen':
+        generate_new_percentile_tables = True
 
-    weight_class_dict = {
-    '53' : {"WeightClass": "53", "Sex" : 'M', "WeightMin": 0.0, "WeightMax": 53.0},
-    '59' : {"WeightClass": "59", "Sex" : 'M', "WeightMin": 52.0, "WeightMax": 59.0},
-    '66' : {"WeightClass": "66", "Sex" : 'M', "WeightMin": 59.0, "WeightMax": 66.0},
-    '74' : {"WeightClass": "74", "Sex" : 'M', "WeightMin": 66.0, "WeightMax": 74.0},
-    '83' : {"WeightClass": "83", "Sex" : 'M', "WeightMin": 74.0, "WeightMax": 83.0},
-    '93' : {"WeightClass": "93", "Sex" : 'M', "WeightMin": 83.0, "WeightMax": 93.0},
-    '105' : {"WeightClass": "105", "Sex" : 'M', "WeightMin": 93.0, "WeightMax": 105.0},
-    '120' : {"WeightClass": "120", "Sex" : 'M', "WeightMin": 105.0, "WeightMax": 120.0},
-    '120+' : {"WeightClass": "120+", "Sex" : 'M', "WeightMin": 120.0, "WeightMax": 999.0},
-    '43' : {"WeightClass": "43", "Sex" : 'F', "WeightMin": 0.0, "WeightMax": 43.0},
-    '47' : {"WeightClass": "47", "Sex" : 'F', "WeightMin": 43.0, "WeightMax": 47.0},
-    '52' : {"WeightClass": "52", "Sex" : 'F', "WeightMin": 47.0, "WeightMax": 52.0},
-    '57' : {"WeightClass": "57", "Sex" : 'F', "WeightMin": 52.0, "WeightMax": 57.0},
-    '63' : {"WeightClass": "63", "Sex" : 'F', "WeightMin": 57.0, "WeightMax": 63.0},
-    '72' : {"WeightClass": "72", "Sex" : 'F', "WeightMin": 63.0, "WeightMax": 72.0},
-    '84' : {"WeightClass": "84", "Sex" : 'F', "WeightMin": 72.0, "WeightMax": 84.0},
-    '84+' : {"WeightClass": "84+", "Sex" : 'F', "WeightMin": 84.0, "WeightMax": 999.0},
-}
+    if generate_new_percentile_tables:
+        s_time = time.time()
+        generate_tables()    
+        e_time = time.time()
+        print("Generating the datatables took: ", (e_time-s_time), "seconds")
 
-# 17 weight classes, make a per
-   
     main_menu = TerminalMenu(['Start', "Exit"], title = "-------------------")
     main_menu_index = None
 
@@ -169,8 +227,24 @@ def main():
         main_menu_index = main_menu.show()
         
         if main_menu_index == 0:
-            weight_classes = ['59','66','74','83','93','105','120','120+','----','43','47','52','57','63','72','84','84+']
-            menu_weight_class = TerminalMenu(weight_classes)
+            # weight_classes = ['59','66','74','83','93','105','120','120+','----','43','47','52','57','63','72','84','84+', '-----',
+            # '52','56','60','67.5','75','82.5','90','100','110','125','140','140+', '-----',
+            # '44','48','52','56','60','67.5','75','82.5','90','90+']
+
+            #weight_classes = list(weight_class_dict.keys())
+            weight_classes_all = list(weight_class_dict.keys()) + list(weight_class_dict_untested.keys())
+            
+            gender_menu = TerminalMenu(['Male', "Female"])
+            gender_selection_index = gender_menu.show()
+            gender_selection = ['M','F'][gender_selection_index]
+
+            tested_menu = TerminalMenu(['Tested', "Untested"])
+            tested_selection_index = tested_menu.show()
+            tested_selection = ['tested','untested'][tested_selection_index]
+
+            weight_classes = [e for e in weight_classes_all if gender_selection in e]
+
+            menu_weight_class = TerminalMenu(weight_classes, title = "Choose Weight Class")
             weight_class_selection = menu_weight_class.show()
 
             lift_options = ['Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg', 'TotalKg']
@@ -181,14 +255,18 @@ def main():
             target_weight = float(input("Your lift (kg)?: "))
 
             weight_class = weight_classes[weight_class_selection]
-            weight_class_full = weight_class_dict[weight_class]
+            
+            try:
+                weight_class_full = weight_class_dict_untested[weight_class]
+            except:
+                weight_class_full = weight_class_dict[weight_class]
+
+
             print(weight_class)
             print(weight_class_full)
 
             lift_selection = lift_options[lift_selection_index]
             print(lift_selection)
-
-
             print(target_weight)
             calc_percentile(weight_class_full, lift_selection, target_weight)
             
@@ -200,33 +278,3 @@ if __name__== '__main__':
     main()
 
 #breakpoint()
-
-
-
-
-
-
-
-
-
-
-# // IPF Men.
-# assert_kg_to_lbs("53", "116");
-# assert_kg_to_lbs("59", "130");
-# assert_kg_to_lbs("66", "145");
-# assert_kg_to_lbs("74", "163");
-# assert_kg_to_lbs("83", "183");
-# assert_kg_to_lbs("93", "205");
-# assert_kg_to_lbs("105", "231");
-# assert_kg_to_lbs("120", "264");
-# assert_kg_to_lbs("120+", "264+")
-
-# // IPF Women.
-# assert_kg_to_lbs("43", "94");
-# assert_kg_to_lbs("47", "103");
-# assert_kg_to_lbs("52", "114");
-# assert_kg_to_lbs("57", "125");
-# assert_kg_to_lbs("63", "138");
-# assert_kg_to_lbs("72", "158");
-# assert_kg_to_lbs("84", "185");
-# assert_kg_to_lbs("84+", "185+");
