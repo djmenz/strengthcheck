d9feac521dc063cf5dab767d6a2f101f21ac7984
daniel.menz <daniel.j.menz@gmail.com>
2023-04-09 13:04:09 +1000

    minor updates

--- gen_data.sh
+++ gen_data.sh
@@ -1,23 +1,40 @@
-# Get latest data and build new ui and deploy (must be run in /strengthcheck folder)
-rm openipf-latest.zip
-rm openpowerlifting-latest.zip
+# remove current datafiles
 rm openipf-latest.csv
 rm openpowerlifting-latest.csv
+
+cd temp_data
 
 wget https://openpowerlifting.gitlab.io/opl-csv/files/openipf-latest.zip
 wget https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip
 
 unzip openipf-latest.zip
 unzip openpowerlifting-latest.zip
-mv openipf*/*.csv openipf-latest.csv
-mv openpowerlifting*/*.csv openpowerlifting-latest.csv
+
+cd ..
+
+mv temp_data/openipf*/*.csv openipf-latest.csv
+mv temp_data/openpowerlifting*/*.csv openpowerlifting-latest.csv
 
 ### cleanup remove directories and zip files
-python3 /home/daniel/Documents/opl/strengthcheck/processer.py gen; 
-cp /home/daniel/Documents/opl/strengthcheck/all_data.json /home/daniel/Documents/opl/strengthcheck-ui/all_data.json
+rm -rf temp_data/*
 
-### need to figure out how to run this command from any directory
-# change date in aboutview.vue
-cd /home/daniel/Documents/opl/strengthcheck-ui
+python3 /Users/djmenz/Documents/opl/strengthcheck/processer.py gen; 
+cp /Users/djmenz/Documents/opl/strengthcheck/all_data.json /Users/djmenz/Documents/opl/strengthcheck-ui/all_data.json
+
+cd /Users/djmenz/Documents/opl/strengthcheck-ui
+
+# fix up date
+sed "111s/.*/<strong>Last Update $(date +"%d %B %Y") - This page relies on data/" src/App.vue > src/App.vue.new
+mv src/App.vue.new src/App.vue
+rm src/App.vue.new
+
+# build distribution
 npm run build
-# aws s3 sync /home/daniel/Documents/opl/strengthcheck-ui/dist s3://jcore.rocks
+
+# run this manually to do the actual sync
+aws s3 sync /Users/djmenz/Documents/opl/strengthcheck-ui/dist s3://jcore.rocks
+
+# required python packages
+# pandas
+# simple_term_menu
+# and npm / npm install
